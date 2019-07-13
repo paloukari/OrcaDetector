4e7ef9ac8554385f07766269276790ecba24cce9
root <paloukari@hotmail.com>
2019-07-13 05:08:43 +0000

    live feed listener

--- orca_detector/live_feed_listener.py
+++ orca_detector/live_feed_listener.py
@@ -5,13 +5,14 @@ Script to listen to a live audio feed. The audio can either be played in the sys
 
 W251 (Summer 2019) - Spyros Garyfallos, Ram Iyer, Mike Winton
 """
-
+import argparse
 import m3u8
 import numpy as np
 import orca_params
 import os
 import random
 import time
+from threading import Thread
 import uuid
 import urllib.request
 
@@ -19,71 +20,83 @@ import urllib.request
 stream_bases = {
     'OrcasoundLab': 'https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_orcasound_lab',
     'BushPoint': 'https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_bush_point',
-    'PortTownsend': 'https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_bush_point/hls/1562956219/live.m3u8'
+    'PortTownsend': 'https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_port_townsend'
 }
 
-def _play_audio(audio_url, output_path, segmentDurationSeconds):
+
+def _play_audio(audio_url, iteration_seconds):
     """
     Uses ffmpeg (via CLI) to retrieve an audio segment from audio_url
-    and play it to the default audio output
+    and play it to the default audio output device for the 
+    specified duration.
     """
     print(f'Playing audio segment: {audio_url}')
 
-    ffmpeg_cli = f'ffmpeg -y -i {audio_url} -t {segmentDurationSeconds} -f pulse "stream name" -loglevel warning'
+    ffmpeg_cli = f'''ffmpeg -y -i {audio_url} -t 
+        {iteration_seconds} -f pulse "stream name" -loglevel warning'''
     os.system(ffmpeg_cli)
 
 
-def _save_audio(audio_url, output_path, segmentDurationSeconds):
+def _save_audio_segments(stream_url, stream_name, segment_seconds, iteration_seconds, output_path):
     """
-    Uses ffmpeg (via CLI) to retrieve an audio segment from audio_url
-    and save it to the local output_path.
+    Uses ffmpeg (via CLI) to retrieve audio segments from audio_url
+    and saves it to the local output_path.
     """
 
-    # generate random file names in case stream reuses its names
-    file_name = f"{uuid.uuid4().hex}.wav"
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    file_name = f"{stream_name}_%02d.wav"
     output_file = os.path.join(output_path, file_name)
-    print(f'Saving audio segment: {audio_url}')
+    print(f'Saving audio segments: {stream_url}')
     print(f' to {output_file}')
 
-    ffmpeg_cli = f'ffmpeg -y -i {audio_url} {output_file} -t {segmentDurationSeconds} -loglevel warning'
+    ffmpeg_cli = f'ffmpeg -y -i {stream_url} -t {iteration_seconds} -f segment -segment_time {segment_seconds} {output_file}' #-loglevel warning'
     os.system(ffmpeg_cli)
 
 
-def collect(streamName, save=False, data_path=orca_params.DATA_PATH):
+def record(stream_name, segment_seconds, sleep_seconds, iteration_seconds, live_feed_path=orca_params.LIVE_FEED):
     """
-    Connects to audio streams in the `streams` dictionary, selects a random segment
-    to record, then sleeps for 1-15 minutes before repeating.  The loop never
-    ends, so to exit, press CTRL-C or kill the process.
+    Connects to specified audio stream(s) in the `stream_bases` dictionary, and records audio segments in iterations.
+    Each segment has length=segment_seconds, each iteration has length=iteration_seconds and between iterations, 
+    a break of sleep_seconds happens.
+    The loop never ends, so to exit, press CTRL-C or kill the process.
+    The iterations are required because the latest feed URI will change over time and needs to 
+    be recalcuated.
     """
 
     while True:
-        for stream_name, stream_base in stream_bases.items():
+        threads = []
+        for _stream_name, _stream_base in stream_bases.items():
+            if stream_name != 'All' and stream_name != _stream_name:
+                continue
+
             try:
                 # get the ID of the latest stream and build URL to load
-                latest = f'{stream_base}/latest.txt'
+                latest = f'{_stream_base}/latest.txt'
                 stream_id = urllib.request.urlopen(
                     latest).read().decode("utf-8").replace('\n', '')
-                stream_url = f'{stream_base}/hls/{stream_id}/live.m3u8'
-                output_path = os.path.join(data_path, 'Noise/', stream_name)
-                stream_obj = m3u8.load(stream_url)
-                # pick a single audio segment from the stream
-                if len(stream_obj.segments) > 0:
-                    audio_segment = random.choice(stream_obj.segments)
-                    base_path = audio_segment.base_uri
-                    file_name = audio_segment.uri
-                    audio_url = base_path + file_name
-                    if save:
-                        _save_audio(audio_url, output_path)
-                    else:
-                        _play_audio(audio_url, output_path)
+                stream_url = '{}/hls/{}/live.m3u8'.format(
+                    (_stream_base), (stream_id))
+                # os.path.join(live_feed_path, stream_name)
+                output_path = live_feed_path
+
+                thread = Thread(target=_save_audio_segments, args=(stream_url, _stream_name,
+                                                              segment_seconds, iteration_seconds,
+                                                              output_path,))
+                threads.append(thread)
+                thread.start()
             except:
                 print(f'Unable to load stream from {stream_url}')
 
-        # sleep for 1-15 minutes
-        sleep_sec = np.random.randint(low=60, high=900)
-        print(
-            f'Sleeping for {sleep_sec} seconds before getting next sample.\n')
-        time.sleep(sleep_sec)
+            
+
+        _ = [t.join() for t in threads]
+
+        if sleep_seconds > 0:
+            print(
+                f'Sleeping for {sleep_seconds} seconds before starting next interation.\n')
+            time.sleep(sleep_seconds)
 
 
 if __name__ == '__main__':
@@ -92,42 +105,46 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='OrcaDetector - W251 (Summer 2019)',
                                      epilog='by Spyros Garyfallos, Ram Iyer, Mike Winton')
 
-    parser.add_argument('--streamName',
+    parser.add_argument('--stream_name',
                         type=str,
-                        choices=['OrcasoundLab', 'BushPoint', 'PortTownsend'],
+                        choices=['OrcasoundLab', 'BushPoint', 'PortTownsend', 'All'],
                         help='Specify the hydrophone live feed stream to listen to.')
 
-    parser.add_argument('--save', action='store_true',
-                        help='Save to files instead of playing.')
-
-    parser.add_argument('--segmentDurationSeconds',
+    parser.add_argument('--segment_seconds',
                         type=int,
-                        help='Defines how many seconds ech iteration segment will be.')
+                        help='Defines how many seconds each audio segment will be.')
 
-    parser.add_argument('--sleepSeconds',
+    parser.add_argument('--sleep_seconds',
                         type=int,
                         help='Seconds to sleep between each iteration.')
 
+    parser.add_argument('--iteration_seconds',
+                        type=int,
+                        help='Total seconds for each iteration.')
+
     args = parser.parse_args()
 
-    if not args.streamName:
-        streamName = 'BushPoint'
+    if not args.stream_name:
+        stream_name = 'All'
     else:
-        streamName = parser.streamName
+        stream_name = parser.stream_name
 
-    if not args.segmentDurationSeconds:
-        segmentDurationSeconds = orca_params.LIVE_FEED_SLEEP_SEGMENT_SECONDS
+    if not args.segment_seconds:
+        segment_seconds = orca_params.LIVE_FEED_SEGMENT_SECONDS
     else:
-        segmentDurationSeconds = parser.segmentDurationSeconds
+        segment_seconds = parser.segment_seconds
 
-    if not args.sleepSeconds:
-        sleepSeconds = orca_params.LIVE_FEED_SLEEP_SECONDS
+    if not args.sleep_seconds:
+        sleep_seconds = orca_params.LIVE_FEED_SLEEP_SECONDS
     else:
-        sleepSeconds = parser.sleepSeconds
+        sleep_seconds = parser.sleep_seconds
 
-    if args.save:
-        collect(streamName=streamName, segmentDurationSeconds=segmentDurationSeconds,
-                sleepSeconds=sleepSeconds, save=True)
+    if not args.iteration_seconds:
+        iteration_seconds = orca_params.LIVE_FEED_ITERATION_SECONDS
     else:
-        collect(streamName=streamName,
-                segmentDurationSeconds=segmentDurationSeconds,  sleepSeconds=sleepSeconds)
+        iteration_seconds = parser.iteration_seconds
+
+    record(stream_name=stream_name,
+           segment_seconds=segment_seconds,
+           sleep_seconds=sleep_seconds,
+           iteration_seconds=iteration_seconds)
